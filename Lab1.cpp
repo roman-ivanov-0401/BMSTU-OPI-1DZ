cb4fa59a3410bc946db7a7e6ddb030870db50348
roman-ivanov-0401 <romanivanov0401@gmail.com>
2023-04-05 06:16:34 +0300

    Generate matrix

---
+++ Lab1.cpp
@@ -0,0 +1,39 @@
+﻿#include <iostream>
+#include <ctime>
+#include <iomanip>
+#include <windows.h>
+#include <cmath>
+
+using namespace std;
+const int MATRIX_SIZE = 17;
+
+// PREPARING
+void GenerateMatrix(int matrix[MATRIX_SIZE][MATRIX_SIZE]);
+int GenerateRandomNumberInRange(int range);
+
+
+int main() {
+	srand(time(NULL));
+
+	int matrix[MATRIX_SIZE][MATRIX_SIZE]{};
+	GenerateMatrix(matrix);
+	RenderMenu(matrix);
+}
+
+// Сгенерировать матрицу 17х17, состоящую из случайных чисел в 
+// диапазоне [-12, 12]
+void GenerateMatrix(int matrix[MATRIX_SIZE][MATRIX_SIZE]) {
+	for (int i = 0; i < MATRIX_SIZE; i++) {
+		for (int j = 0; j < MATRIX_SIZE; j++)
+		{
+			matrix[i][j] = GenerateRandomNumberInRange(12);
+		}
+	}
+}
+
+// Сгенерировать случайное число в диапазоне [-12; 12]
+int GenerateRandomNumberInRange(int range) {
+	return rand() % (range + 1) * 2 - range;
+
+	if (!nullElementIsfound) cout << "Null element nto found" << endl;
+}
